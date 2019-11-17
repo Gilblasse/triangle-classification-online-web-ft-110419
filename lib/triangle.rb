ee290f06d7aad2e90383f68683cb862b3967da02
Nethelbert <gilblasse@gmail.com>
2019-11-17 04:47:38 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,28 +8,26 @@ class Triangle
   
   def kind 
     inequality_check
-        check_equality = all_sides.each_with_object({}) do |v,h| 
-                        		h[v] ||= 0 
-                        		h[v] += 1
-                      	 end.values.max
+    triangle_type = all_sides.each_with_object({}) do |v,h| 
+                    		h[v] ||= 0 
+                    		h[v] += 1
+                  	 end.values.max
           
-        case check_equality
-          when 3
-            :equilateral
-          when 2
-            :isosceles
-          when 1
-            :scalene 
-        end
+    case triangle_type
+      when 3
+        :equilateral
+      when 2
+        :isosceles
+      when 1
+        :scalene 
     end
+  end
     
     def inequality_check
-       triangle_check = all_sides.any? {|num| num == 0 || num.angle.class == Float}
-       
+      a,b,c = all_sides
+      inequality_therom = [a < b + c, b < a + c, c < a + b].any? false
+      raise TriangleError if inequality_therom
     end
-end
-  
-  
   
   class TriangleError < StandardError
   end
